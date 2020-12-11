8d1a1259162c8a565cd690bbc091c8c010c8922b
kamel <kamelmalk96@gmail.com>
2020-12-11 21:23:19 +0100

    fix duplication some data + latest jar

--- python_script/get_playlists_tracks.py
+++ python_script/get_playlists_tracks.py
@@ -2,7 +2,7 @@ import spotipy
 import json
 from datetime import date
 
-cred = spotipy.SpotifyClientCredentials(client_id="cliend_id", client_secret="client_secret")
+cred = spotipy.SpotifyClientCredentials(client_id='client_id', client_secret='client_secret')
 
 sp = spotipy.Spotify(client_credentials_manager=cred)
 
@@ -13,19 +13,21 @@ countries_list = ['AD', 'AR', 'AU', 'AT', 'BE', 'BO', 'BR', 'BG', 'CA', 'CL', 'C
 
 today = date.today().strftime("%Y-%m-%d")
 
-file = 'raw_data/playlists/{}.txt'.format(today)
-
+file = '{}.txt'.format(today)
+s = set()
 with open(file, "w") as f:
     for country in countries_list:
         play = [(pl['name'], pl['id']) for pl in sp.category_playlists("toplists", country)['playlists']['items']]
         for p in play:
-            playlist = sp.playlist(p[1])
-            track_list = [track['track'] for track in playlist['tracks']['items'] if track['track'] is not None]
-            for track in track_list:
-                track['artists'] = track['artists'][0]
-                track['playlist'] = {
-                    "id": playlist['id'],
-                    "name": playlist['name']
-                }
-                json.dump(track, f, sort_keys=True)
-                f.write('\n')
+            if p[1] not in s:
+                s.add(p[1])
+                playlist = sp.playlist(p[1])
+                track_list = [track['track'] for track in playlist['tracks']['items'] if track['track'] is not None]
+                for track in track_list:
+                    track['artists'] = track['artists'][0]
+                    track['playlist'] = {
+                        "id": playlist['id'],
+                        "name": playlist['name']
+                    }
+                    json.dump(track, f, sort_keys=True)
+                    f.write('\n')
