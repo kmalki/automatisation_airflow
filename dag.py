54634301edc9dfa4f8191ef15ada5507db497576
kamel <kamelmalk96@gmail.com>
2020-12-11 14:28:27 +0100

    change path db

--- dag.py
+++ dag.py
@@ -37,7 +37,8 @@ dag = DAG(
 spark_playlists = BashOperator(
     task_id="spark_playlists",
     bash_command="HADOOP_CONF_DIR=/etc/hadoop/conf HADOOP_USER_NAME=groupe5 spark-submit --deploy-mode cluster --master yarn --class Main "
-                 "/root/airflow/dags/groupe5/automatisation_airflow/spark_jar/spotify_playlists_ingestion_2.12-1.0.jar ",
+                 "/root/airflow/dags/groupe5/automatisation_airflow/spark_jar/spotify_playlists_ingestion_2.12-1.0.jar "
+                 "-spark.yarn.dist.files hdfs:///user/groupe5/hive-site.xml ",
     dag=dag
 )
 
